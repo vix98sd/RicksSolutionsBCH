e3db9f802c12bae24c7b0cf30d9db3d8c34b7cf6
lazarstarco <lazarstarco@gmail.com>
2020-12-11 22:00:51 +0100

    development script fix

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 // const mysql = require('./middleware/database')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.render('index', {data: req.query})
+})
 
 app.listen(port, () => {
     process.stdout.write("\u001b[2J\u001b[0;0H");
