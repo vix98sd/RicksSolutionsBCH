3b16adda4ee35c0863c90c1ca38f97d42ebea5da
lazarstarco <lazarstarco@gmail.com>
2020-12-12 14:12:08 +0100

    clean up

--- routers/page3.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = new express.Router()
-
-router.get('/page3', (req, res) => {
-    res.send('I am page 3')
-})
-
-module.exports = router
--- routers/page4.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = new express.Router()
-
-router.get('/page4', (req, res) => {
-    res.send('I am page 4')
-})
-
-module.exports = router
