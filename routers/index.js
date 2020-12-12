eafff470503d2a26a20ed780a42251e0d81381ac
lazarstarco <lazarstarco@gmail.com>
2020-12-12 10:24:02 +0100

    index.js changes

--- routers/index.js
+++ routers/index.js
@@ -8,11 +8,22 @@ const pool = mysql.getPool()
 router.get('/', (req, res) => {
     pool.getConnection((error, connection) => {
         if (error) throw error
-        const sql = 'SELECT 2 + 2'
+        const sql = 'SELECT * FROM dummytable'
         if (error) throw error
         connection.query(sql, (error, result) => {
             if (error) throw error
             res.send(result)
+        })
+    })
+})
+router.get('/:name', (req, res) => {
+    pool.getConnection((error, connection) => {
+        if (error) throw error
+        const sql = 'SELECT * FROM dummytable'
+        if (error) throw error
+        connection.query(sql, (error, result) => {
+            if (error) throw error
+            res.send(`${req.params.name} is a champion`)
         })
     })
 })
