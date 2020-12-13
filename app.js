45cd1834042acc852ca76b4858b5c6e994b70e79
lazarstarco <lazarstarco@gmail.com>
2020-12-13 04:15:34 +0100

    finishing touches... for now

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.listen(port, () => {
     process.stdout.write("\u001b[2J\u001b[0;0H");
     const currentdate = new Date();
     const datetime =
-        `Started: ${(currentdate.getDay() + 1)}/${(currentdate.getMonth() + 1)}/${currentdate.getFullYear()} @ ${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`
+        `Started: ${(currentdate.getDate() + 1)}/${(currentdate.getMonth() + 1)}/${currentdate.getFullYear()} @ ${currentdate.getHours()}:${currentdate.getMinutes()}:${currentdate.getSeconds()}`
     console.log(datetime)
 
     console.log(`Server running on port ${port}`)
